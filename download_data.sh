a0079223698408b662c0486e473bd395b5feaf1a
Rizwan Nato <rizwanato@hotmail.fr>
2022-03-26 11:28:53 +0100

    Changed data location

--- download_data.sh
+++ download_data.sh
@@ -1,2 +1,4 @@
 . ./credential
 wget --user $USER --password $PWD -r -np -R "index.html*" https://isip.piconepress.com/projects/tuh_eeg/downloads/tuh_eeg_seizure/v1.5.2/
+cp -R isip.piconepress.com/projects/tuh_eeg/downloads/tuh_eeg_seizure/ .
+rm -R isip.piconepress.com/
