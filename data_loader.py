fe81542ddfaca25c89803a5b5552084cafc665ed
Fransou <philippe.formont@student-cs.fr>
2022-04-06 18:43:30 +0200

    Full architecture (classical conv) and training pipeline blueprint

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,5 @@
 from os import path
+import os
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,13 +14,19 @@ from sklearn.metrics import f1_score
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset, DataLoader
+import yaml 
+import networkx as nx
 
 
+with open('config.yaml') as f:
+    cfg = yaml.load(f)
+
+ROOT_DIR = cfg['ROOT_DIR']
 
 class FaceLandmarksDataset(Dataset):
     """Face Landmarks dataset."""
 
-    def __init__(self, csv_file, root_dir= , transform=None):
+    def __init__(self, csv_file, root_dir= ROOT_DIR, transform=None, mode='train'):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -27,29 +34,31 @@ class FaceLandmarksDataset(Dataset):
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
-        self.landmarks_frame = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
+        self.paths = []
+        self.labels = []
+        for path, subdirs, files in os.walk(root_dir):
+            for name in files:
+                path_split = path.split("\\")
+                if path_split[1] == mode:
+                    if not path in self.paths:
+                        self.paths.append(path)
+                        self.labels.append(path_split[2])
 
     def __len__(self):
-        return len(self.landmarks_frame)
+        return len(self.labels)
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+        final_graph = nx.empty_graph()
+        for path, subdirs, files in os.walk(self.paths[idx]):
+            G = nx.read_gpickle(os.path.join(path,files))
+            final_graph = nx.full_join(final_graph, G)
+        
 
-        img_name = os.path.join(self.root_dir,
-                                self.landmarks_frame.iloc[idx, 0])
-        image = io.imread(img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:]
-        landmarks = np.array([landmarks])
-        landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
 
-        if self.transform:
-            sample = self.transform(sample)
 
-        return sample
+        return from_networkx(final_graph)
 
 
 
--- model/architecture.py
+++ model/architecture.py
@@ -13,13 +13,13 @@ from sklearn.metrics import f1_score
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset, DataLoader
-
-
+import torch.nn as nn
+from dgl.nn import MaxPooling
 
 
 class BasicGraphModel(nn.Module):
 
-    def __init__(self, g, n_layers, input_size, hidden_size, output_size, nonlinearity):
+    def __init__(self, g, n_layers, input_size, hidden_size, output_size, nonlinearity,dropout_p=0.5):
         super().__init__()
 
         self.g = g
@@ -29,19 +29,22 @@ class BasicGraphModel(nn.Module):
             self.layers.append(GraphConv(hidden_size, hidden_size, activation=nonlinearity))
         self.layers.append(GraphConv(hidden_size, output_size))
 
+        self.dropout = nn.Dropout(p = dropout_p)
+        self.maxpool = MaxPooling()
+        self.FC = nn.Linear(output_size, 7)
+
     def forward(self, inputs):
         outputs = inputs
         for i, layer in enumerate(self.layers):
             outputs = layer(self.g, outputs)
-
-        return outputs
-
-
-
+        outputs = F.relu(self.dropout(outputs))
+        outputs = self.maxpool(outputs)
+        outputs = self.FC(outputs)
+        return F.softmax(outputs)
 
 class AttGraphModel(nn.Module):
 
-    def __init__(self, g, input_size, hidden_size, output_size, nonlinearity, n_attention=2, heads = 4, d_rate = 0.):
+    def __init__(self, g, input_size, hidden_size, output_size, nonlinearity, n_attention=2, heads = 4, dropout_p = 0.):
         super().__init__()
         self.n_attention = n_attention
         self.g = g
@@ -50,11 +53,14 @@ class AttGraphModel(nn.Module):
 
         for i in range(n_attention-1):
             if i ==0:
-                self.layers.append(GATConv(input_size, channels, num_heads=heads, attn_drop=d_rate, feat_drop=d_rate, activation = nonlinearity))
+                self.layers.append(GATConv(input_size, channels, num_heads=heads, attn_drop=dropout_p, feat_drop=dropout_p, activation = nonlinearity))
             else:
-                self.layers.append(GATConv(channels*heads, channels, num_heads=heads, attn_drop=d_rate, feat_drop=d_rate, activation = nonlinearity))
+                self.layers.append(GATConv(channels*heads, channels, num_heads=heads, attn_drop=dropout_p, feat_drop=dropout_p, activation = nonlinearity))
         self.layers.append(GATConv(channels*heads, output_size, 6))
 
+        self.dropout = nn.Dropout(p = dropout_p)
+        self.maxpool = MaxPooling()
+        self.FC = nn.Linear(output_size, 7)
 
     def forward(self, inputs):
         outputs = inputs
@@ -62,35 +68,7 @@ class AttGraphModel(nn.Module):
             outputs = torch.reshape(outputs,(outputs.shape[0],-1))
             outputs = layer(self.g, outputs)
             
-        outputs = torch.mean(outputs, 1)
-        return outputs
-
-class AttGraphModel_skip(nn.Module):
-
-    def __init__(self, g, input_size, hidden_size, output_size, nonlinearity, n_attention=2, heads = 4, d_rate = 0.):
-        super().__init__()
-        self.n_attention = n_attention
-        self.g = g
-        self.layers = nn.ModuleList()
-        channels = hidden_size//heads
-
-        for i in range(n_attention-1):
-            if i ==0:
-                self.layers.append(GATConv(input_size, channels, num_heads=heads, attn_drop=d_rate, feat_drop=d_rate, activation = nonlinearity))
-            else:
-                self.layers.append(GATConv(channels*heads, channels, num_heads=heads, attn_drop=d_rate, feat_drop=d_rate, activation = nonlinearity))
-        self.layers.append(GATConv(channels*heads, output_size, 6))
-
-
-    def forward(self, inputs):
-        outputs = inputs
-        for i, layer in enumerate(self.layers):
-            if i%3 == 1:
-                skip = outputs
-            outputs = torch.reshape(outputs,(outputs.shape[0],-1))
-            outputs = layer(self.g, outputs)
-            if i%3 == 2:
-                outputs = outputs + skip
-            
-        outputs = torch.mean(outputs, 1)
-        return outputs
+        outputs = F.relu(self.dropout(outputs))
+        outputs = self.maxpool(outputs)
+        outputs = self.FC(outputs)
+        return F.softmax(outputs)
